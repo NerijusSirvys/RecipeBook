ca54dbf27a20a77901c6a84d085d64d4748cc52e
Nerijus Sirvys <n.sirvys@senso.cloud>
2025-06-29 20:55:05 +0300

    Add project files.

---
+++ RecipeBook.Mobile/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace RecipeBook.Mobile
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
